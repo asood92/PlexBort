be00f2826a7837aaac3934c8a6d16ab0dbb21f04
Amman Sood <392075+asood92@users.noreply.github.com>
2022-03-16 13:29:06 -0400

    added help functionality

--- app.py
+++ app.py
@@ -14,3 +14,13 @@ async def on_message(message):
 
     if message.content.startswith('!ping'):
         await message.channel.send("Pong!")
+
+    if message.content.startswith('!help'):
+        await message.channel.send("Hi, I'm PlexBort! \n\
+            I'm a statistics bot, and I can do a few things! Commands are: \n \
+                !ping - to get my status \n \
+                !help - to get this message \n \
+                !recentlyadded - to get the latest additions to Plex \n \
+            ")
+
+client.run(os.getenv('TOKEN'))
